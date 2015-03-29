09f9700f54b477c252308d26cf799199ac45eb34
Bruno <brunocoann@gmail.com>
2015-03-29 20:59:51 -0300

    botoes coloridos

--- app/pedidos.js
+++ app/pedidos.js
@@ -1,24 +1,49 @@
  function ListaPedidosController($scope) {
+ 	Parse.initialize("8EuvtU751IPG6V7whmY0Ot8EHHcrQOKwehqZS54b", "D0BwOlwWL63eUEHo12KHkpNZg1NJr9jKkzPPq9Jc");	
+
     $scope.itens = [
-        {telefone: '976840689', status: 'Preparando'}
+        {telefone: '976840689', status: 'Preparando', btnClass: 'btn btn-danger'}
     ];
 
     $scope.adicionaItem = function () {
     $scope.itens.push({telefone: $scope.item.telefone,
-                       status:'Preparando'});
+                       status:'Preparando', btnClass: 'btn btn-danger'});
      $scope.item.telefone = ' ';
      $scope.sendPush();
     };
 
     $scope.sendPush = function () {
-		Parse.initialize("8EuvtU751IPG6V7whmY0Ot8EHHcrQOKwehqZS54b", "D0BwOlwWL63eUEHo12KHkpNZg1NJr9jKkzPPq9Jc");	
 
 	    var query = new Parse.Query(Parse.Installation);
 		 query.equalTo('deviceType', 'android');
 		Parse.Push.send({
 		  where: query, // Set our Installation query
 		  data: {
-		    alert: "send notification by js."		  
+		    alert: "seu pedido está sendo preparado."		  
+			}
+		}, {
+		  success: function() {
+		    // Push was successful
+		  },
+		  error: function(error) {
+		    // Handle error
+		  }
+		});
+    
+    };
+
+        $scope.sendPush2 = function (n) {
+
+        $scope.itens[n].status = 'saiu para entrega';
+        $scope.itens[n].btnClass ='btn btn-warning';
+
+
+	    var query = new Parse.Query(Parse.Installation);
+		 query.equalTo('deviceType', 'android');
+		Parse.Push.send({
+		  where: query, // Set our Installation query
+		  data: {
+		    alert: "seu pedido saiu para entrega."		  
 			}
 		}, {
 		  success: function() {
